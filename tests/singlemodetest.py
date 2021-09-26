14e47f79b1d8d305a0ecb7b245f0b8191d8d04a9
Phionx <shantanu.rajesh.jha@gmail.com>
2021-09-25 21:53:56 -0400

    added tests against scipy.integrate.odeint

---
+++ tests/singlemodetest.py
@@ -0,0 +1,52 @@
+"""
+Unittests
+
+Run using: python -m unittest tests/singlemodetest.py
+"""
+import os
+import sys
+import unittest
+
+sys.path.insert(0, ".." + os.sep)
+
+from quantumnetworks import SingleModeSystem
+from scipy.integrate import odeint
+import numpy as np
+
+
+class SingleModeTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.params = {"omega_a": 1, "kappa_a": 0.001}
+
+    def test_forward_euler_no_A_in(self):
+        A_in = lambda t: 0
+        x_0 = np.array([1, 0])
+        ts = np.linspace(0, 10, 100001)
+
+        # solve using SystemSolver
+        sys = SingleModeSystem(params=self.params, A_in=A_in)
+        X = sys.forward_euler(x_0, ts)
+
+        # solve using scipy.integrate.odeint
+        func = lambda y, t: sys.eval_f(y, sys.eval_u(t))
+        sol = odeint(func, x_0, ts)
+        self.assertTrue(np.allclose(X.T, sol, atol=0.002))
+
+    def test_forward_euler_default_A_in(self):
+        A_in = None
+        x_0 = np.array([1, 0])
+        ts = np.linspace(0, 10, 100001)
+
+        # solve using SystemSolver
+        sys = SingleModeSystem(params=self.params, A_in=A_in)
+        X = sys.forward_euler(x_0, ts)
+
+        # solve using scipy.integrate.odeint
+        func = lambda y, t: sys.eval_f(y, sys.eval_u(t))
+        sol = odeint(func, x_0, ts)
+        self.assertTrue(np.allclose(X.T, sol, atol=0.002))
+
+
+#%%
+if __name__ == "__main__":
+    unittest.main()
