60e03e6d1f23ed2fc6648d1e751fcac539a2b7d6
Phionx <shantanu.rajesh.jha@gmail.com>
2021-10-10 20:33:37 -0400

    identity system quantity

--- quantumnetworks/analysis/quantities/stats.py
+++ quantumnetworks/analysis/quantities/stats.py
@@ -6,6 +6,11 @@ import numpy as np
 from quantumnetworks.analysis.quantities.base import SystemQuantity
 
 
+class Identity(SystemQuantity):
+    def calculate(self, xs: np.ndarray) -> np.ndarray:
+        return xs
+
+
 class Average(SystemQuantity):
     def calculate(self, xs: np.ndarray) -> np.ndarray:
         return np.average(xs, axis=1)
